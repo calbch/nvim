30e86fd25099eba71cf0d95348e4a21246e91155
caldotdev <cluckenbach@protonmail.com>
2024-06-22 07:06:04 +0200

    feat: allow disabling of the autoformat feature on a per file basis

--- lua/cal/init.lua
+++ lua/cal/init.lua
@@ -1,5 +1,6 @@
 require("cal.options")
 require("cal.remap")
 require("cal.autocmds")
+require("cal.usercmds")
 
 require("cal.lazy_init")
--- lua/cal/plugins/autoformat.lua
+++ lua/cal/plugins/autoformat.lua
@@ -16,6 +16,12 @@ return {
     notify_on_error = false,
     format_on_save = function(bufnr)
       local disable_filetypes = { c = false }
+
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
