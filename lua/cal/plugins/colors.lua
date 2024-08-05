186c386641eeff590d0b8e745a6c6dd5265ffb66
calbch <calbch@protonmail.com>
2024-08-05 17:02:03 +0200

    add color schemes

--- lua/cal/plugins/colors.lua
+++ lua/cal/plugins/colors.lua
@@ -1,20 +1,42 @@
 return {
   {
+    "ellisonleao/gruvbox.nvim",
+    -- priority = 1000,
+    -- config = true,
+
+    -- init = function()
+    --   vim.cmd.colorscheme("1980_sun")
+    -- end,
+  },
+  {
     dir = "~/Code/1980_sun",
+    dependencies = { "rktjmp/lush.nvim" },
+    -- lazy = false,
+    -- priority = 1000,
+
+    -- init = function()
+    --   vim.cmd.colorscheme("1980_sun")
+    -- end,
+  },
+  {
+    "slugbyte/lackluster.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme("lackluster")
+    --   -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+    --   -- vim.cmd.colorscheme("lackluster-mint")
+    -- end,
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
     lazy = false,
     priority = 1000,
-
     init = function()
-      vim.cmd.colorscheme("1980_sun")
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("zenwritten")
+      vim.g.zenwritten = { lightness = "dim" }
     end,
   },
-  -- "slugbyte/lackluster.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- init = function()
-  --   vim.cmd.colorscheme("lackluster")
-  --   -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-  --   -- vim.cmd.colorscheme("lackluster-mint")
-  -- end,
-  { "rktjmp/lush.nvim" },
 }
