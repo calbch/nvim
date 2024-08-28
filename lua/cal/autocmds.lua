945a2d1a156388932e971a1ac34364ed005f1a4c
calbch <calbch@protonmail.com>
2024-08-28 16:52:56 +0200

    annoy the fuck out of me if I spam movement keys

--- lua/cal/autocmds.lua
+++ lua/cal/autocmds.lua
@@ -6,54 +6,41 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
+vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    if vim.fn.argc() == 0 and vim.fn.line2byte(1) == -1 then
-      local ascii_arr = [[
-                        .__  ___.            .__     
-          ____  _____   |  | \_ |__    ____  |  |__  
-        _/ ___\ \__  \  |  |  | __ \ _/ ___\ |  |  \ 
-        \  \___  / __ \_|  |__| \_\ \\  \___ |   Y  \
-         \___  >(____  /|____/|___  / \___  >|___|  /
-             \/      \/           \/      \/      \/ 
-      ]]
+    local keys = { "h", "j", "k", "l" }
+    local last_press = {}
+    local press_count = {}
+    local spam_threshold = 0.5 -- seconds
 
-      local ascii_art = vim.split(ascii_arr, "\n")
-
-      -- Get the editor's width and height
-      local width = vim.api.nvim_get_option("columns")
-      local height = vim.api.nvim_get_option("lines")
-
-      -- Calculate center
-      local row_start = math.floor((height - #ascii_art) / 2)
-      local col_start = math.floor((width - #ascii_art[1]) / 2)
-
-      -- Clear the buffer
-      vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
-
-      -- Add padding rows
-      for _ = 1, row_start do
-        vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
-      end
-
-      -- Set the ASCII art in the buffer
-      for _, line in ipairs(ascii_art) do
-        local padded_line = string.rep(" ", col_start) .. line
-        vim.api.nvim_buf_set_lines(0, -1, -1, false, { padded_line })
-      end
-
-      -- Set buffer options
-      vim.bo.buftype = "nofile"
-      vim.bo.bufhidden = "wipe"
-      vim.bo.swapfile = false
-      vim.bo.modifiable = false
-
-      -- Hide line numbers
-      vim.wo.number = false
-      vim.wo.relativenumber = false
-
-      -- Hide the cursor
-      vim.opt.guicursor = "n:none"
+    local function speak(text)
+      -- This command uses 'say' on macOS. Adjust for other operating systems.
+      vim.fn.jobstart({ "say", text }, { detach = true })
     end
+
+    local function create_mapping(modes)
+      for _, key in ipairs(keys) do
+        vim.keymap.set(modes, key, function()
+          local current_time = vim.loop.hrtime() / 1e9
+          local count = vim.v.count1
+
+          if last_press[key] and (current_time - last_press[key] < spam_threshold) and count == 1 then
+            press_count[key] = (press_count[key] or 0) + 1
+            if press_count[key] > 2 then
+              speak("move properly bozo!")
+              return ""
+            end
+          else
+            press_count[key] = 1
+          end
+
+          last_press[key] = current_time
+          return key
+        end, { noremap = true, expr = true })
+      end
+    end
+
+    -- Create mappings for normal and visual modes
+    create_mapping({ "n", "v" })
   end,
 })
