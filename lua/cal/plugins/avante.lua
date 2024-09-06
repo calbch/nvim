2fe38f87b29041719d89184ca1fcc7b2dc2f60c8
calbch <calbch@protonmail.com>
2024-09-06 17:24:23 +0200

    update avante config

--- lua/cal/plugins/avante.lua
+++ lua/cal/plugins/avante.lua
@@ -1,15 +1,37 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  lazy = false,
+  version = false,
   opts = {
-    -- add any opts here
+    windows = {
+      width = 45,
+    },
   },
+  build = "make",
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below is optional, make sure to setup it properly if you have lazy=true
+    -- Optional
+    "nvim-tree/nvim-web-devicons",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
     {
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
