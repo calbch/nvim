434f10e9358c40402b621c7647d007196cd0a370
calbch <calbch@protonmail.com>
2024-11-12 18:02:26 +0100

    update lockfiles and change color theme

--- lua/cal/plugins/colors.lua
+++ lua/cal/plugins/colors.lua
@@ -1,42 +1,43 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    -- priority = 1000,
-    -- config = true,
-
-    -- init = function()
-    --   vim.cmd.colorscheme("1980_sun")
-    -- end,
-  },
-  {
-    dir = "~/Code/1980_sun",
-    dependencies = { "rktjmp/lush.nvim" },
-    -- lazy = false,
-    -- priority = 1000,
-
-    -- init = function()
-    --   vim.cmd.colorscheme("1980_sun")
-    -- end,
-  },
-  {
-    "slugbyte/lackluster.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme("lackluster")
-    --   -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-    --   -- vim.cmd.colorscheme("lackluster-mint")
-    -- end,
-  },
-  {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    lazy = false,
     priority = 1000,
+    config = true,
+
     init = function()
-      vim.o.background = "dark"
-      vim.cmd.colorscheme("zenwritten")
-      vim.g.zenwritten = { lightness = "dim" }
+      -- vim.o.background = "light"
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
+  --   {
+  --     dir = "~/Code/1980_sun",
+  --     dependencies = { "rktjmp/lush.nvim" },
+  --     lazy = false,
+  --     priority = 1000,
+  --
+  --     init = function()
+  --       vim.cmd.colorscheme("1980_sun")
+  --     end,
+  --   },
+  --   {
+  --     "slugbyte/lackluster.nvim",
+  --     lazy = false,
+  --     priority = 1000,
+  --     init = function()
+  --       vim.cmd.colorscheme("lackluster")
+  --       -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+  --       -- vim.cmd.colorscheme("lackluster-mint")
+  --     end,
+  --   },
+  --   {
+  --     "zenbones-theme/zenbones.nvim",
+  --     dependencies = { "rktjmp/lush.nvim" },
+  --     lazy = false,
+  --     priority = 1000,
+  --     init = function()
+  --       vim.o.background = "dark"
+  --       vim.cmd.colorscheme("zenwritten")
+  --       vim.g.zenwritten = { lightness = "dim" }
+  --     end,
+  --   },
 }
